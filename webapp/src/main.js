008ff49f4e8393a556464e0d24a0fa6c7baeffc6
Jean Sarlôn <jean.sarlon@icloud.com>
2019-06-10 22:09:04 -0300

    vue initial setup with axios

--- webapp/src/main.js
+++ webapp/src/main.js
@@ -3,8 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import icons from './utils/icons'
 
 Vue.config.productionTip = false
+Vue.use(icons)
 
 /* eslint-disable no-new */
 new Vue({
